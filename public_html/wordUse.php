33282e38e044dc681e08e1661be307e575da33f2
James <james@jarofgreen.co.uk>
2012-04-28 15:43:39 +0100

    have to search for a word

--- public_html/wordUse.php
+++ public_html/wordUse.php
@@ -10,8 +10,13 @@ require dirname(__FILE__).'/../src/global.php';
 
 $tpl = getSmarty();
 
-$search = new WordUseSearchAllUsers();
-$tpl->assign('wordSearch',$search);
+if ($_POST && isset($_POST['search'])) {
+	$search = new WordUseSearchAllUsers();
+	$search->byWord($_POST['search']);
+	$tpl->assign('wordSearch',$search);
+} else {
+	$tpl->assign('wordSearch',null);
+}
 
 $tpl->display('wordUse.htm');
 
--- src/WordUseSearchAllUsers.class.php
+++ src/WordUseSearchAllUsers.class.php
@@ -8,20 +8,30 @@
 
 class WordUseSearchAllUsers extends BaseSearch {
 	
+	protected $searchForWord;
 	
-	
+	public function byWord($word) {
+		$this->searchForWord = $word;
+	}
+
 	protected function execute() {
 		if ($this->searchDone) throw new Exception("Search already done!");
 		$db = getDB();
 		$where = array();
 		$joins = array();
+		$vars = array();
 				
+		if ($this->searchForWord) {
+			$where[] = " word_use.word LIKE :term";
+			$vars['term'] = '%'.$this->searchForWord.'%';
+		}
+		
 		$sql = "SELECT word_use.word, SUM(word_use.num_used) AS num_used ".
 			"FROM word_use ".implode(" ", $joins).(count($where) > 0 ? " WHERE ".implode(" AND ", $where) : "").
 			" GROUP BY word_use.word ";
 		//die($sql);
 		$stat = $db->prepare($sql);
-		$stat->execute();
+		$stat->execute($vars);
 		while($d = $stat->fetch(PDO::FETCH_ASSOC)) {
 			$this->results[] = $d;
 		}
