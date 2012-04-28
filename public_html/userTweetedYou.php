ae9d1d98929bf6facc5b892cd1eacb395b8c2105
James <james@jarofgreen.co.uk>
2012-04-28 10:16:59 +0100

    Who tweeted who? Not sure these are the right way round tho; to early and not enought breakfast!

---
+++ public_html/userTweetedYou.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package Flocklight
+ * @license GPLv3
+ * @see https://github.com/jarofgreen/flocklight
+ */
+require dirname(__FILE__).'/../src/global.php';
+
+
+$user = TwitterUser::findByUserName($_GET['id']);
+if (!$user) die('not found');
+
+$tpl = getSmarty();
+$tpl->assign('user',$user);
+
+$search = new TwitterUserSearch();
+$search->tweetsTo($user);
+$tpl->assign('searchUsers',$search);
+
+$tpl->display('userTweetedYou.htm');
+
+
